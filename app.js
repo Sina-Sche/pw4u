3670ac4754ed077ae2dadfbbf8f042c99e78ca93
Sina-Sche <si.scheckenbach@gmail.com>
2020-11-15 16:57:09 +0100

    :sparkles: Add functionality
    Add delete function and chalk colors

--- app.js
+++ app.js
@@ -1,29 +1,60 @@
-const { readCommandLineArguments } = require("./lib/commandLine");
-const { getPassword } = require("./lib/passwords");
-const { askForMasterPassword } = require("./lib/questions");
+const { readCommandLineArguments, welcomeUser } = require("./lib/commandLine");
+const {
+  getPassword,
+  setPassword,
+  searchforPw,
+  setNewPassword,
+  deletePw,
+} = require("./lib/passwords");
+const {
+  askForMasterPassword,
+  askForAction,
+  doubleCheck,
+} = require("./lib/questions");
+const { close, connectToPwdDb } = require("./lib/database");
 const { isMasterPasswordCorrect } = require("./lib/validation");
+const chalk = require("chalk");
 
 async function run() {
+  await welcomeUser();
   const masterPassword = await askForMasterPassword();
-
-  if (!isMasterPasswordCorrect(masterPassword)) {
-    console.error("You are not welcome here! 👿 Try again!");
-    return run();
+  if (!(await isMasterPasswordCorrect(masterPassword))) {
+    console.error(chalk.red("You better get out of here 👊!"));
+    process.exit(1);
   }
+  console.log(chalk.blue("Connecting to database...🚀"));
 
+  await connectToPwdDb();
   const [passwordName, newPasswordValue] = readCommandLineArguments();
   if (!passwordName) {
-    console.error("Missing password name!");
-    return process.exit(9);
-  }
+    const { action } = await askForAction();
+    console.log(chalk.blue.underline(`You chose to ${action} a password`));
 
+    if (action === "search") {
+      await searchforPw(passwordName);
+    } else if (action === "add") {
+      await setNewPassword(passwordName);
+    } else if (action === "delete") {
+      await deletePw(passwordName);
+    }
+  }
   if (newPasswordValue) {
-    await setPassword(passwordName, newPasswordValue);
-    console.log(`Password ${passwordName} set 🎉`);
-  } else {
-    const passwordValue = await getPassword(passwordName);
-    console.log(`Your password is ${passwordValue} 🎉`);
+    const answer = await doubleCheck();
+    if (answer === true) {
+      await setPassword(passwordName, newPasswordValue);
+      console.log(
+        chalk.green(`You added/updated the password for ${passwordName} 🔐`)
+      );
+    } else {
+      console.log(chalk.red("What do you want, you dummy???"));
+    }
+    // } else {
+    //   await getPassword(passwordName);
   }
+  if (passwordName) {
+    await getPassword(passwordName);
+  }
+  // await close();
 }
-
+// }
 run();
