b8d22ac94ad4a08d02c214cd099e0b96b21aa312
Sina-Sche <si.scheckenbach@gmail.com>
2020-11-16 15:57:00 +0100

    :sparkles: Post method
    Introduce Post Method and start using nodemon

--- server.js
+++ server.js
@@ -1,34 +1,54 @@
 const express = require("express");
-const { connectToPwdDb, findPwfor } = require("./lib/database");
-const { getPassword } = require("./lib/passwords");
+const { connectToPwdDb, deleteOne } = require("./lib/database");
+const { getPassword, setPassword } = require("./lib/passwords");
+
 const app = express();
+app.use(express.json());
 const port = 3000;
 const chalk = require("chalk");
 
 app.get("/api/passwords/:name", async (request, response) => {
   const { name } = request.params;
-  const passwordValue = await getPassword(name);
-  response.send(passwordValue);
-});
-app.put("/api/passwords/add/:name/:value", async (request, response) => {
-  const { name } = request.params;
-  const { value } = request.params;
-  const encryptedValue = CryptoJS.AES.encrypt(
-    value,
-    await readMasterPassword()
-  ).toString();
-  const collection = await setCollection("passwords");
-  const newPwObject = {
-    name: name,
-    value: encryptedValue,
-  };
-  await replaceOne(collection, newPwObject);
-  response.send("You added a new password");
+  try {
+    const passwordValue = await getPassword(name);
+    if (!passwordValue) {
+      response
+        .status(404)
+        .send("Could not find the password you were looking for!");
+      return;
+    }
+    response.send(passwordValue);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send("An internal server error occured!");
+  }
 });
 
-app.delete("/api/passwords/delete/:name", async (request, response) => {
-  app.get;
-  await deleteOne(request.name);
+app.post("/api/passwords/", async (request, response) => {
+  const password = request.body;
+  try {
+    await setPassword(password.name, password.value);
+    response.send(`You added a password for ${password.name}`);
+  } catch (error) {
+    console.error(error);
+    response.status(500).send("An unexpected error occured. Try again later");
+  }
+  // const encryptedValue = CryptoJS.AES.encrypt(
+  //   value,
+  //   await readMasterPassword()
+  // ).toString();
+  // const collection = await setCollection("passwords");
+  // const newPwObject = {
+  //   name: name,
+  //   value: encryptedValue,
+  // };
+  // await replaceOne(collection, newPwObject);
+  // response.send("You added a new password");
+});
+
+app.delete("/api/passwords/:name", async (request, response) => {
+  const { name } = request.params;
+  await deleteOne(name);
   response.send("Delete request of password");
 });
 
